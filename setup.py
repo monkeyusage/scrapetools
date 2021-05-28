05d679a6f07a96596688a8674b19bce9e5b87340
monkey usage <monkeyusage@gmail.com>
2021-05-28 16:39:11 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="scrapetools",
-    version="0.2.0",
+    version="0.2.1",
     description="A library for scraping more easily",
     url="https://github.com/monkeyusage/scrapetools",
     author="monkeyusage",
