6d35fa23776f9e0bb76903405dbaa132b6c2d4bd
monkey usage <monkeyusage@gmail.com>
2021-06-17 15:02:13 +0200

    change logging and validation

--- scrapetools/credentials.py
+++ scrapetools/credentials.py
@@ -3,21 +3,21 @@ from scraper_api import ScraperAPIClient
 from os import environ, path, mkdir
 import logging
 
-if not path.exists(".scrapetools"):
-    mkdir(".scrapetools")
+API_KEY = environ.get("scraperapi_proxy")
+if API_KEY is None:
+    raise EnvironmentError(
+        "Did not find proxy secret key environment variable <scraperapi_proxy> inside the system"
+    )
+
+CLIENT = ScraperAPIClient(API_KEY)
+
+if not path.exists("debug"):
+    mkdir("debug")
 
 logging.basicConfig(
-    filename=".scrapetools/debug.log",
+    filename="debug/debug.log",
+    format="%(asctime)s %(levelname)s %(message)s",
     filemode="a",
     datefmt="%Y-%m-%d %H:%M:%S",
     level="DEBUG",
 )
-
-API_KEY = environ.get("scraperapi_proxy")
-if API_KEY is None:
-    logging.error("Could not find proxy environment variable")
-    raise EnvironmentError(
-        "Did not find proxy secret key environment variable inside the system"
-    )
-
-CLIENT = ScraperAPIClient(API_KEY)
--- scrapetools/validation.py
+++ scrapetools/validation.py
@@ -4,11 +4,10 @@ from random import randint
 
 def validate_url(url: str) -> None:
     """simple http protocol check"""
-    if not url.startswith("http"):
-        raise ValueError("url should use http protocol")
+    assert url.startswith("http"), f"url should use http protocol. Got {url}"
 
 
-def validate_sleep(**kwargs: int) -> int:
+def validate_kwargs(**kwargs: int) -> int:
     """
     validates kwargs entered when fetching for online data
     the current setup accepts the following parameters:
@@ -16,23 +15,33 @@ def validate_sleep(**kwargs: int) -> int:
         - max_t for max time sleeping
         - sleep for exact amount of time sleeping
     all of these arguments are integers representing seconds
+    """
+    ALLOWED_KWARGS = set([
+        "min_t",
+        "max_t",
+        "sleep",
+    ])
+    assert all(map(lambda kw: kw in ALLOWED_KWARGS, kwargs.items())), \
+        f"Unauthorized key word argument used, use one of the following: {ALLOWED_KWARGS}"
+
+    assert all(map(lambda _, v: isinstance(int, v), kwargs.items())), \
+        "All keyword values should be integers"
+
+def get_sleeping_time(**kwargs:int) -> int:
+    """
     if sleep is provided we just return it
     else if we get both min_t and max_t and min_t < max_t
       we randomly choose a number in that range and return it
     otherwise we randomly choose between 10 and 20
     """
-    for key, value in kwargs.items():
-        if not isinstance(value, int):
-            warnings.warn("key word arguments for fetch function are specific to sleep parameters")
-            warnings.warn("the validation function accepts: min_t, max_t and sleep")
-            raise ValueError(f"Sleep argument {key} must be an int")
     min_t: int | None = kwargs.get("min_t")
     max_t: int | None = kwargs.get("max_t")
     sleep: int | None = kwargs.get("sleep")
 
-    if sleep:
+    if sleep is not None:
         return sleep
-    elif (min_t is not None) and (max_t is not None) and (min_t < max_t):
+    elif (min_t is not None) and (max_t is not None):
+        assert min_t < max_t, "min_t must be smaller than max_t"
         return randint(min_t, max_t)
     return randint(10, 20)
 
@@ -42,4 +51,6 @@ def validate_params(url: str, **kwargs: int) -> int:
     validates url then sleeping kwargs
     """
     validate_url(url)
-    return validate_sleep(**kwargs)
+    validate_kwargs(**kwargs)
+    sleeping_time = get_sleeping_time(**kwargs)
+    return sleeping_time
